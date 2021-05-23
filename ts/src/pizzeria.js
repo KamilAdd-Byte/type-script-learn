92d0cd811f66245bde403ccb010f99f5a37a1744
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-05-23 17:08:36 +0200

    1.1.1 - Utworzenie klasy TS porównanie z klasa JS

---
+++ ts/src/pizzeria.js
@@ -0,0 +1,8 @@
+// klasa TS struktura
+var Pizzeria = /** @class */ (function () {
+    function Pizzeria(name) {
+        this.name = name;
+    }
+    return Pizzeria;
+}());
+var LaStrada = new Pizzeria('LaStrada');
---
+++ ts/src/pizzeria.ts
@@ -0,0 +1,9 @@
+// klasa TS struktura
+class Pizzeria {
+    name;
+
+    constructor(name) {
+        this.name = name
+    }
+}
+const  LaStrada = new Pizzeria('LaStrada');
