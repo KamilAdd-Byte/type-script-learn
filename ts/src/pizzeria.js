07b1767a902fc0a56ff6ba3e156191db8fb74bea
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-05-23 17:30:40 +0200

    1.1.2 - Struktura klasy TS + geterry settery construktor

--- ts/src/pizzeria.js
+++ ts/src/pizzeria.js
@@ -1,8 +1,64 @@
 // klasa TS struktura
 var Pizzeria = /** @class */ (function () {
+    // konstruktor
     function Pizzeria(name) {
+        this._pizzasInOrder = [];
+        this._maxPizzaIsOven = 10;
+        this._recipes = [];
+        this._manager = 'Kamil';
         this.name = name;
     }
+    Object.defineProperty(Pizzeria.prototype, "pizzasInOrder", {
+        //getteri i settery
+        get: function () {
+            return this._pizzasInOrder;
+        },
+        set: function (value) {
+            this._pizzasInOrder = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Pizzeria.prototype, "maxPizzaIsOven", {
+        get: function () {
+            return this._maxPizzaIsOven;
+        },
+        set: function (value) {
+            this._maxPizzaIsOven = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Pizzeria.prototype, "recipes", {
+        get: function () {
+            return this._recipes;
+        },
+        set: function (value) {
+            this._recipes = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Pizzeria.prototype, "manager", {
+        get: function () {
+            return this._manager;
+        },
+        set: function (value) {
+            this._manager = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    //metoda
+    Pizzeria.prototype.order = function (pizza) {
+        this._pizzasInOrder.push(pizza);
+    };
+    Pizzeria.prototype.isOvenFull = function () {
+        return this._pizzasInOrder.length > this._maxPizzaIsOven;
+    };
     return Pizzeria;
 }());
-var LaStrada = new Pizzeria('LaStrada');
+var laStrada = new Pizzeria('La Strada');
+laStrada.order('Hawajska');
+laStrada.manager = 'Mas';
+console.log(laStrada);
--- ts/src/pizzeria.ts
+++ ts/src/pizzeria.ts
@@ -1,9 +1,62 @@
 // klasa TS struktura
 class Pizzeria {
-    name;
 
+    //field - pola klasy
+    readonly name;
+    private _pizzasInOrder = [];
+    private _maxPizzaIsOven = 10;
+    private _recipes = [];
+    private _manager = 'Kamil';
+
+    // konstruktor
     constructor(name) {
         this.name = name
     }
+
+    //getteri i settery
+
+
+    get pizzasInOrder(): any[] {
+        return this._pizzasInOrder;
+    }
+
+    set pizzasInOrder(value: any[]) {
+        this._pizzasInOrder = value;
+    }
+
+    get maxPizzaIsOven(): number {
+        return this._maxPizzaIsOven;
+    }
+
+    set maxPizzaIsOven(value: number) {
+        this._maxPizzaIsOven = value;
+    }
+
+    get recipes(): any[] {
+        return this._recipes;
+    }
+
+    set recipes(value: any[]) {
+        this._recipes = value;
+    }
+
+    get manager(): string {
+        return this._manager;
+    }
+
+    set manager(value: string) {
+        this._manager = value;
+    }
+
+//metoda
+    order(pizza){
+        this._pizzasInOrder.push(pizza);
+    }
+    private isOvenFull(){
+        return this._pizzasInOrder.length > this._maxPizzaIsOven;
+    }
 }
-const  LaStrada = new Pizzeria('LaStrada');
+const laStrada = new Pizzeria('La Strada');
+laStrada.order('Hawajska');
+laStrada.manager = 'Mas'
+console.log(laStrada);
