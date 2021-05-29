1f87dff42381289a4e78d2cd313aea9525e37162
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-05-29 15:04:10 +0200

    4.3 - Typ Tuple. Definiowanie tablicy o różnych typach

---
+++ ts/dist/tuple-type.js
@@ -0,0 +1,6 @@
+"use strict";
+// Dla definiowania tablic o różnych typach.
+//zadeklarowanie tablicy:  var nazwa: [typ, typ, typ] = [przypisz wartość, przypisz wartość, przypisz wartość]
+var mixins = ['John', 36, false];
+//dodanie wartości do tablicy:
+mixins[3] = { a: 3 };
---
+++ ts/src/tuple-type.ts
@@ -0,0 +1,7 @@
+// Dla definiowania tablic o różnych typach.
+
+//zadeklarowanie tablicy:  var nazwa: [typ, typ, typ] = [przypisz wartość, przypisz wartość, przypisz wartość]
+var mixins: [string,number,boolean] = ['John',36,false];
+
+//dodanie wartości do tablicy:
+mixins[3]= {a: 3};
